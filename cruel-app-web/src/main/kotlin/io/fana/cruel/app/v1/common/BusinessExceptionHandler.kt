ea9022b7341093ce0809d07aea2c3e5329a17225
Chunghwan <chunghwan@syllabs.io>
2022-10-01 13:32:17 +0900

    feat: add business exception controller advice

---
+++ cruel-app-web/src/main/kotlin/io/fana/cruel/app/v1/common/BusinessExceptionHandler.kt
@@ -0,0 +1,28 @@
+package io.fana.cruel.app.v1.common
+
+import io.fana.cruel.app.common.logger
+import io.fana.cruel.core.exception.BusinessException
+import io.fana.cruel.core.exception.HttpErrorResponse
+import io.fana.cruel.core.exception.client.ClientException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import java.rmi.ServerError
+
+@ControllerAdvice
+class BusinessExceptionHandler {
+    private val log = logger()
+
+    @ExceptionHandler(BusinessException::class)
+    fun handleBusinessException(exception: BusinessException): ResponseEntity<HttpErrorResponse> {
+        when (exception) {
+            is ClientException -> log.warn("ClientException", exception.message)
+            is ServerError -> {
+                log.error("ServerException", exception.message)
+            }
+        }
+        return ResponseEntity
+            .status(exception.statusCode.value)
+            .body(HttpErrorResponse.of(exception.message!!, exception.errorCode))
+    }
+}
