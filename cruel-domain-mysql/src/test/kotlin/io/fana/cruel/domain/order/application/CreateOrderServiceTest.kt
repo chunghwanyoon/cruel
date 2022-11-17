6ebfd67be1df573a77dd119256f8b3ea8db6e085
Chunghwan <chunghwan@syllabs.io>
2022-11-17 14:20:36 +0900

    test: add CreateOrderServiceTest

---
+++ cruel-domain-mysql/src/test/kotlin/io/fana/cruel/domain/order/application/CreateOrderServiceTest.kt
@@ -0,0 +1,57 @@
+package io.fana.cruel.domain.order.application
+
+import com.appmattus.kotlinfixture.kotlinFixture
+import io.fana.cruel.core.type.OrderStatus
+import io.fana.cruel.domain.order.OrderTerm
+import io.fana.cruel.domain.order.domain.Order
+import io.fana.cruel.domain.order.domain.OrderRepository
+import io.fana.cruel.domain.user.application.GetUserService
+import io.fana.cruel.domain.user.domain.User
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+
+internal class CreateOrderServiceTest : BehaviorSpec({
+    isolationMode = IsolationMode.InstancePerLeaf
+
+    val orderRepository = mockk<OrderRepository>()
+    val getUserService = mockk<GetUserService>()
+    val createOrderService = CreateOrderService(
+        orderRepository = orderRepository,
+        getUserService = getUserService,
+    )
+    val fixture = kotlinFixture()
+    val userFixture = fixture<User>()
+    val validOrderTerm = 3
+    val content = ""
+    val orderFixture = fixture<Order> {
+        factory<OrderTerm> { OrderTerm(validOrderTerm) }
+        property(Order::content) { content }
+    }
+    val orderRequest = CreateOrderRequest(
+        userId = userFixture.id,
+        amount = orderFixture.amount,
+        term = validOrderTerm,
+        content = content,
+    )
+
+    given("주문 요청이 주어졌을 때") {
+        every { getUserService.getUserById(userFixture.id) } returns userFixture
+
+        `when`("주문을 생성하면") {
+            every { orderRepository.save(orderFixture) } returns orderFixture
+
+            then("주문이 생성된다") {
+                val order = createOrderService.requestOrder(orderRequest)
+
+                order.user.id shouldBe userFixture.id
+                order.amount shouldBe orderRequest.amount
+                order.term shouldBe validOrderTerm
+                order.content shouldBe orderRequest.content
+                order.status shouldBe OrderStatus.CREATED
+            }
+        }
+    }
+})
