4f8e63cecae537fc322fc1b7d6cbd0f2563d6e1c
Chunghwan <chunghwan@syllabs.io>
2022-11-20 18:39:04 +0900

    feat: 상환 계산 도메인 추가

---
+++ cruel-domain-common/src/test/kotlin/io/fana/cruel/domain/repayment/CalculateRepaymentServiceTest.kt
@@ -0,0 +1,8 @@
+package io.fana.cruel.domain.repayment
+
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.BehaviorSpec
+
+internal class CalculateRepaymentServiceTest : BehaviorSpec({
+    isolationMode = IsolationMode.InstancePerLeaf
+})
